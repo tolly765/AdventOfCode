1565b8e410e520f5e57f692a927a97e672a2523c
Tom <tom@riptidewave.com>
2021-12-02 11:11:49 +0000

    Day 1 Task 2

--- 2021/Day01/sonardepth.py
+++ 2021/Day01/sonardepth.py
@@ -1,9 +1,11 @@
 input = open("input.txt", "r")
-sonarLines = input.readlines()
+sonarLines = input.read().splitlines()
+sonarLines = list(map(int, sonarLines))
 
-task1Count = 1 # First line is always bigger than 0
+task1Count = 0
 lineCount = len(sonarLines)
 
+# Task 1
 i = 0
 while i <= lineCount:
     try:
@@ -13,4 +15,19 @@ while i <= lineCount:
             task1Count = task1Count + 1
     except IndexError:
         print(f"Total greater than previous: {task1Count}")
+        break
+    i += 1
+
+#Task 2
+i = 0
+task2Count = 0
+currentSum = 10000
+while i <= lineCount:
+    try:
+        sumLines = sonarLines[i] + sonarLines[i+1] + sonarLines[i+2] # Sliding Window
+        if sumLines > currentSum:
+            task2Count = task2Count + 1
+        currentSum = sumLines
+    except IndexError:
+        print(f"Total greater than previous window: {task2Count}")
     i += 1
