a04eadfc7d45c47233e7283df1578f4e3f6b3e98
Tom <tom@riptidewave.com>
2021-12-06 13:59:43 +0000

    Day 02 Task 2

--- 2021/Day02/sonardirections.py
+++ 2021/Day02/sonardirections.py
@@ -7,9 +7,9 @@ lineCount = len(sonarLines)
 depth = 0
 horiz = 0
 
+# Task 1
 i = 0
 while i < lineCount:
-    
     try:
         splitLine = sonarLines[i].split()
         if 'forward' in splitLine[0]:
@@ -27,5 +27,29 @@ while i < lineCount:
     except Exception as err:
         print(err)
         
+    i = i + 1
+print(f' Horiz: {horiz} \n Depth: {depth} \n Multiply: {horiz * depth}')
+
+# Task 2
+depth = 0
+horiz = 0
+aim = 0
+
+i = 0
+
+while i < lineCount:
+    try:
+        splitLine = sonarLines[i].split()
+        if 'forward' in splitLine[0]:
+            horiz = horiz + int(splitLine[1])
+            depth = depth + (aim * int(splitLine[1]))
+        elif 'up' in splitLine[0]:
+            aim = aim - int(splitLine[1])
+        elif 'down' in splitLine[0]:
+            aim = aim + int(splitLine[1])
+        else:
+            pass
+    except Exception as err:
+        print(err)
     i = i + 1
 print(f' Horiz: {horiz} \n Depth: {depth} \n Multiply: {horiz * depth}')
