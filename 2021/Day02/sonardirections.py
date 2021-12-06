11ba746f7592091a564ab26f67ccab7f87a80762
Tom <tom@riptidewave.com>
2021-12-06 11:10:43 +0000

    Day 02 Task 1

---
+++ 2021/Day02/sonardirections.py
@@ -0,0 +1,31 @@
+input = open("input.txt", "r")
+sonarLines = input.read().splitlines()
+# sonarLines = list(map(int, sonarLines))
+
+task1Count = 0
+lineCount = len(sonarLines) 
+depth = 0
+horiz = 0
+
+i = 0
+while i < lineCount:
+    
+    try:
+        splitLine = sonarLines[i].split()
+        if 'forward' in splitLine[0]:
+            print("Hit Forward")
+            horiz = horiz + int(splitLine[1])
+        elif 'up' in splitLine[0]:
+            print("Hit Up")
+            depth = depth - int(splitLine[1])
+
+        elif 'down' in splitLine[0]:
+            print("Hit Down")
+            depth = depth + int(splitLine[1])
+        else:
+            pass
+    except Exception as err:
+        print(err)
+        
+    i = i + 1
+print(f' Horiz: {horiz} \n Depth: {depth} \n Multiply: {horiz * depth}')
